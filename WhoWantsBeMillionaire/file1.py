5c4f74a142f793f865cd97c308f13dcda321fae6
lisafox1505 <222975926+lisafox1505@users.noreply.github.com>
2026-01-12 22:31:23 +0200

    add new parts of code

--- WhoWantsBeMillionaire/file1.py
+++ WhoWantsBeMillionaire/file1.py
@@ -27,7 +27,7 @@ TEXT_COLOR = (245, 255, 250)
 NAME_WIN_COLOR = (255, 215, 0)
 WINS_COLORS_RECT = (255, 255, 255)
 SUMA_SELECTOR_COLOR = (255, 140, 0)
-HINT_COLOR = (135, 206, 235)
+HINT_COLOR = (135, 206, 250)
 HINT_TEXT_COLOR = (25, 25, 112)
 
 #Координати питання відповіді підказки
@@ -46,21 +46,40 @@ BORDER_C = pygame.Rect(415, 400, 300, 60)
 RECT_D = pygame.Rect(435, 500, 260, 40)
 BORDER_D = pygame.Rect(415, 490, 300, 60)
 
-RECT_H_1 = pygame.Rect(50, 150, 60, 40)
-RECT_H_2 = pygame.Rect(50, 210, 60, 40)
-RECT_H_3 = pygame.Rect(50, 230, 60, 40)
-RECT_H_4 = pygame.Rect(50, 250, 60, 40)
+RECT_H_1 = pygame.Rect(50, 150, 110, 30)
+RECT_H_2 = pygame.Rect(50, 200, 110, 30)
+RECT_H_3 = pygame.Rect(170, 150, 110, 30)
+RECT_H_4 = pygame.Rect(170, 200, 110, 30)
 
 #Суми виграшу, дані стану
 PRIZES = [0, 100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000]
 step = 0
 
-#Тексти питання відповіді
+#Тексти питання відповіді підказки
 q_text = FONT_Q.render("Как правильно учить пайтон?", True, TEXT_COLOR)
 a_text = FONT_ANS.render("A: Практиковаться", True, TEXT_COLOR)
 b_text = FONT_ANS.render("B: Только смотреть видео", True, TEXT_COLOR)
 c_text = FONT_ANS.render("C: Спать с книгой под подушкой", True, TEXT_COLOR)
 d_text = FONT_ANS.render("D: Удалить PyCharm", True, TEXT_COLOR)
+hint_text_1 = FONT_ANS.render("50/50", True, HINT_TEXT_COLOR)
+hint_text_2 = FONT_ANS.render("Допомога залу", True, HINT_TEXT_COLOR)
+hint_text_3 = FONT_ANS.render("Дзвінок другу", True, HINT_TEXT_COLOR)
+hint_text_4 = FONT_ANS.render("Вихід", True, HINT_TEXT_COLOR)
+
+# Списки для циклу створення кнопок
+answers_list = [
+    (RECT_A, BORDER_A, a_text),
+    (RECT_B, BORDER_B, b_text),
+    (RECT_C, BORDER_C, c_text),
+    (RECT_D, BORDER_D, d_text)
+]
+
+hints_list = [
+    (RECT_H_1, hint_text_1),
+    (RECT_H_2, hint_text_2),
+    (RECT_H_3, hint_text_3),
+    (RECT_H_4, hint_text_4)
+]
 
 running = True
 while running:
@@ -96,31 +115,25 @@ while running:
     else:
         selector_index = 9 - (step - 6)
 
-    # Рисуем каретку (оранжевый прямоугольник с закруглением)
+    # Малюємо виділення для виграшних сум
     selector_y = y_start_selector + (selector_index * spacing)
     rect_coordinat = pygame.Rect(600, selector_y, 180, 20)
     pygame.draw.rect(screen, SUMA_SELECTOR_COLOR, rect_coordinat, border_radius=8, width=2)
 
-#Прямокутники з питанням та варіантами відповіді
+#   Прямокутники з питанням та варіантами відповіді
     pygame.draw.rect(screen, BUTTON_FILL_COLOR, RECT_Q)
     pygame.draw.rect(screen, BUTTON_BORDER_COLOR, BORDER_Q, border_radius=30, width=2)
     screen.blit(q_text, q_text.get_rect(center=RECT_Q.center))
 
-    pygame.draw.rect(screen, BUTTON_FILL_COLOR, RECT_A)
-    pygame.draw.rect(screen, BUTTON_BORDER_COLOR, BORDER_A, border_radius=20, width=2)
-    screen.blit(a_text, a_text.get_rect(center=RECT_A.center))
+    for button_rect, border, text in answers_list:
+        pygame.draw.rect(screen, BUTTON_FILL_COLOR, button_rect)
+        pygame.draw.rect(screen, BUTTON_BORDER_COLOR, border, border_radius=30, width=2)
+        screen.blit(text, text.get_rect(center=button_rect.center))
 
-    pygame.draw.rect(screen, BUTTON_FILL_COLOR, RECT_B)
-    pygame.draw.rect(screen, BUTTON_BORDER_COLOR, BORDER_B, border_radius=20, width=2)
-    screen.blit(b_text, b_text.get_rect(center=RECT_B.center))
-
-    pygame.draw.rect(screen, BUTTON_FILL_COLOR, RECT_C)
-    pygame.draw.rect(screen, BUTTON_BORDER_COLOR, BORDER_C, border_radius=20, width=2)
-    screen.blit(c_text, c_text.get_rect(center=RECT_C.center))
-
-    pygame.draw.rect(screen, BUTTON_FILL_COLOR, RECT_D)
-    pygame.draw.rect(screen, BUTTON_BORDER_COLOR, BORDER_D, border_radius=20, width=2)
-    screen.blit(d_text, d_text.get_rect(center=RECT_D.center))
+    #Кнопки підказок
+    for button_rect, text in hints_list:
+        pygame.draw.rect(screen, HINT_COLOR, button_rect, border_radius=20)
+        screen.blit(text, text.get_rect(center=button_rect.center))
 
     pygame.display.flip()
     clock.tick(60)
